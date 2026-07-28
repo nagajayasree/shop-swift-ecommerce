f21050f5aa3e3fa2c2a5a38d0708755fe40593c9
js <js@ramaka.com>
2026-07-28 14:58:29 -0400

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-white">
+       <h3>Home Page</h3>
+    </div>
+  );
+}
